2f46b9a1a3bd592683fd2ac28bfc9226083b3b78
github-insu <93insu@gmail.com>
2026-04-28 18:53:50 +0900

    chore: first commit

---
+++ src/main/java/com/example/simple_graphql_grpc_project_2604/SimpleGraphqlGrpcProject2604Application.java
@@ -0,0 +1,13 @@
+package com.example.simple_graphql_grpc_project_2604;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleGraphqlGrpcProject2604Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleGraphqlGrpcProject2604Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simple_graphql_grpc_project_2604/SimpleGraphqlGrpcProject2604ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simple_graphql_grpc_project_2604;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleGraphqlGrpcProject2604ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
